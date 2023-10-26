33d742af53fb103a1f698d4b97d5445e4e326cf4
25garcial <91680716+25garcial@users.noreply.github.com>
2023-10-26 20:47:52 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function rotate90(){
 function rotate90ccw(){
 
 }
-
+function main(){
 var rotate90Button=document.querySelector("rotate90cc");
 rotate90Button.innerHTML="rotate clockwise";
 rotate90Button.addEventListener("click");
@@ -36,15 +36,21 @@ var rotate90ButtonCCW=document.querySelector("rotate90ccw");
 rotate90ButtonCCW.innerHTML="rotate counter clockwise";
 rotate90ButtonCCW.addEventListener("click");
 
+
 var createMatrixButton=document.querySelector(".submit");
 createMatrixButton.addEventListener("click", ()=>{
+    
     controls.setAttribute("display", "inital");
     var matrixHeight=Number(document.querySelector(".height").textContent);
     var matrixWidth=Number(document.querySelector(".width").textContent);
 
-    document.querySelector(".start").setAttribute("display", "none");
     matrix.innerHTML=createMatrix(matrixHeight,matrixWidth);
+    alert(`Created ${matrixWidth} by ${matrixHeight} matrix`);
+    document.querySelector(".start").setAttribute("display", "none");
 })
 
 controls.appendChild(rotate90Button);
 controls.appendChild(rotate90ButtonCCW);
+}
+
+main();
